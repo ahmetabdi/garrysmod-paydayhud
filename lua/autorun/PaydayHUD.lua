9812d9c33ddbf75a5423144059ac0005e72c620f
Ahmet <ahmetabdi@gmail.com>
2013-12-30 02:21:37 +0000

    Testing pickup circle on duffle bag

--- lua/autorun/PaydayHUD.lua
+++ lua/autorun/PaydayHUD.lua
@@ -1,4 +1,3 @@
-
 if SERVER then
 	AddCSLuaFile()
 	resource.AddSingleFile( "resource/fonts/Aero_Matics_Display.ttf" )
@@ -6,233 +5,249 @@ if SERVER then
 	resource.AddSingleFile( "materials/paydayhud/paydayhud_health_small.png" )
 	resource.AddSingleFile( "materials/paydayhud/paydayhud_armor.png" )
 	resource.AddSingleFile( "materials/paydayhud/paydayhud_armor_small.png" )
+
+	for _, v in pairs( player.GetAll() ) do
+	 if ( v:Armor() == 0 ) then
+	 v:SetArmor( 100 )
+	 end
+	end
+
 else
-
-	hook.Add("HUDPaint", "FC_HUD", function()
-
-		FC_HUD = {}
-
-		------------------
-		-- CONFIG START --
-		------------------
-
-		FC_HUD.TeamBasedHP = false
-		FC_HUD.DefaultTargetInfo = false
-		FC_HUD.LargerStatus = false
-		FC_HUD.ColoredRoleBack = false
-		FC_HUD.ColoredRoleText = false
-		FC_HUD.ShowOnlyHP = false
-
-		------------------
-		-- CONFIG START --
-		------------------
-
-		surface.CreateFont( "FC_HUD_10", { font = "Aero Matics Display Regular", antialias = true, size = 10 } )
-		surface.CreateFont( "FC_HUD_20", { font = "Aero Matics Display Regular", antialias = true, size = 20 } )
-		surface.CreateFont( "FC_HUD_30", { font = "Aero Matics Display Regular", antialias = true, size = 30 } )
-		surface.CreateFont( "FC_HUD_40", { font = "Aero Matics Display Regular", antialias = true, size = 40 } )
-
-		FC_HUD.White = Material("vgui/white")
-		FC_HUD.SmallHP = Material("paydayhud/paydayhud_health_small.png")
-		FC_HUD.BigHP = Material("paydayhud/paydayhud_health.png")
-		FC_HUD.SmallAR = Material("paydayhud/paydayhud_armor_small.png")
-		FC_HUD.BigAR = Material("paydayhud/paydayhud_armor.png")
-
-		FC_HUD.LastHP = nil
-		FC_HUD.HPFallout = {}
-
-		local insert = table.insert
-
-		local function clamp(v) if (v > 1) then return 1 else return v end end
-
-		local function NiceUV(x, y, w, h, perc, flipped)
-
-			if flipped then
-
-				if (prec == 0) then return {} end
-
-				local tbl = {} -- our pizza
-				insert(tbl, {x = x, y = y, u = 0.5, v = 0.5})
-
-				if (perc >= 315) then
-					insert(tbl, {x = x + w - clamp((perc - 315) / 45) * w, y = y - h, u = 1 - clamp((perc - 315) / 45) / 2, v = 0})
-				end
-
-				if (perc >= 225) then
-					insert(tbl, {x = x + w, y = y + h - clamp((perc - 225) / 90) * h * 2, u = 1, v = 1 - clamp((perc - 225) / 90)})
-				end
-
-				if (perc >= 135) then
-					insert(tbl, {x = x - w + clamp((perc - 135) / 90) * h * 2, y = y + h, u = clamp((perc - 135) / 90), v = 1})
-				end
-
-				if (perc >= 45) then
-					insert(tbl, {x = x - w, y = y - h + clamp((perc - 45) / 90) * h * 2, u = 0, v = clamp((perc - 45) / 90)})
-				end
-
-				insert(tbl, {x = x - clamp(perc / 45) * w, y = y - h, u = 0.5 - clamp(perc / 45) / 2, v = 0})
-				insert(tbl, {x = x, y = y - h, u = 0.5, v = 0})
-
-				return tbl
-
-			else
-
-				local tbl = {}
-				insert(tbl, {x = x, y = y, u = 0.5, v = 0.5})
-				insert(tbl, {x = x, y = y - h, u = 0.5, v = 0})
-
-				if (perc > 45) then insert(tbl, {x = x + w, y = y - h, u = 1, v = 0})
-				else				insert(tbl, {x = x + perc / 45 * w, y = y - h, u = 0.5 + clamp(perc / 45) / 2, v = 0}) return tbl
-				end
-
-				perc = perc - 45
-				if (perc > 90) then
-					insert(tbl, {x = x + w, y = y + h, u = 1, v = 1})
-				else
-					insert(tbl, {x = x + w, y = y - h + perc / 90 * h * 2, u = 1, v = clamp(perc / 90)}) return tbl
-				end
-
-				perc = perc - 90
-				if (perc > 90) then
-					insert(tbl, {x = x - w, y = y + h, u = 0, v = 1})
-				else
-					insert(tbl, {x = x + w - perc / 90 * h * 2, y = y + h, u = 1 - clamp(perc / 90), v = 1}) return tbl
-				end
-
-				perc = perc - 90
-				if (perc > 90) then
-					insert(tbl, {x = x - w, y = y - h, u = 0, v = 0})
-				else
-					insert(tbl, {x = x - w, y = y + h - perc / 90 * h * 2, u = 0, v = 1 - clamp(perc / 90)}) return tbl
-				end
-
-				perc = perc - 90
-				insert(tbl, {x = x - w + perc / 45 * w, y = y - h, u = clamp(perc / 45) / 2, v = 0})
-
-				return tbl
-			end
+	-- Removing the default HUD
+	local hud = {"CHudHealth", "CHudBattery", "CHudAmmo", "CHudSecondaryAmmo", "CHudCrosshair", "CHudVoiceStatus"}
+	function HideHUD(name)
+		for k, v in pairs(hud)do
+			if name == v then return false end
 		end
+	end
+	hook.Add("HUDShouldDraw", "HideHUD", HideHUD)
 
-		function CreateBorderedCircle(x, y, size, border, perc, addrot, parts)
+	FC_HUD = {}
 
-			local sin = math.sin
-			local cos = math.cos
-			local rad = math.rad
+	------------------
+	-- CONFIG START --
+	------------------
 
-			local parts = parts or 100
+	FC_HUD.TeamBasedHP = false
+	FC_HUD.DefaultTargetInfo = false
+	FC_HUD.LargerStatus = false
+	FC_HUD.ColoredRoleBack = false
+	FC_HUD.ColoredRoleText = false
+	FC_HUD.ShowOnlyHP = false
+
+	------------------
+	-- CONFIG START --
+	------------------
+
+	surface.CreateFont( "FC_HUD_10", { font = "Aero Matics Display Regular", antialias = true, size = 10 } )
+	surface.CreateFont( "FC_HUD_20", { font = "Aero Matics Display Regular", antialias = true, size = 20 } )
+	surface.CreateFont( "FC_HUD_30", { font = "Aero Matics Display Regular", antialias = true, size = 30 } )
+	surface.CreateFont( "FC_HUD_40", { font = "Aero Matics Display Regular", antialias = true, size = 40 } )
+
+	FC_HUD.White = Material("vgui/white")
+	FC_HUD.SmallHP = Material("paydayhud/paydayhud_health_small.png")
+	FC_HUD.BigHP = Material("paydayhud/paydayhud_health.png")
+	FC_HUD.SmallAR = Material("paydayhud/paydayhud_armor_small.png")
+	FC_HUD.BigAR = Material("paydayhud/paydayhud_armor.png")
+
+	FC_HUD.LastHP = nil
+	FC_HUD.HPFallout = {}
+
+	local insert = table.insert
+
+	local function clamp(v) if (v > 1) then return 1 else return v end end
+
+	local function NiceUV(x, y, w, h, perc, flipped)
+
+		if flipped then
+
+			if (prec == 0) then return {} end
+
+			local tbl = {} -- our pizza
+			insert(tbl, {x = x, y = y, u = 0.5, v = 0.5})
+
+			if (perc >= 315) then
+				insert(tbl, {x = x + w - clamp((perc - 315) / 45) * w, y = y - h, u = 1 - clamp((perc - 315) / 45) / 2, v = 0})
+			end
+
+			if (perc >= 225) then
+				insert(tbl, {x = x + w, y = y + h - clamp((perc - 225) / 90) * h * 2, u = 1, v = 1 - clamp((perc - 225) / 90)})
+			end
+
+			if (perc >= 135) then
+				insert(tbl, {x = x - w + clamp((perc - 135) / 90) * h * 2, y = y + h, u = clamp((perc - 135) / 90), v = 1})
+			end
+
+			if (perc >= 45) then
+				insert(tbl, {x = x - w, y = y - h + clamp((perc - 45) / 90) * h * 2, u = 0, v = clamp((perc - 45) / 90)})
+			end
+
+			insert(tbl, {x = x - clamp(perc / 45) * w, y = y - h, u = 0.5 - clamp(perc / 45) / 2, v = 0})
+			insert(tbl, {x = x, y = y - h, u = 0.5, v = 0})
+
+			return tbl
+
+		else
 
 			local tbl = {}
-			local onerad = rad(360) / parts
-			local fixpos = rad(90)
-			local ret = false
-			local ending = perc * 3.6 * (parts / 360)
-			local innersize = size - border
-			local i = 0
+			insert(tbl, {x = x, y = y, u = 0.5, v = 0.5})
+			insert(tbl, {x = x, y = y - h, u = 0.5, v = 0})
 
-			if (addrot != nil) then
-				fixpos = fixpos + rad(addrot)
+			if (perc > 45) then insert(tbl, {x = x + w, y = y - h, u = 1, v = 0})
+			else				insert(tbl, {x = x + perc / 45 * w, y = y - h, u = 0.5 + clamp(perc / 45) / 2, v = 0}) return tbl
 			end
 
-			while(true) do
-				if (not ret) then
-					table.insert(tbl, {
-						x = x - cos(i * onerad - fixpos) * innersize,
-						y = y + sin(i * onerad - fixpos) * innersize
-					})
-
-					i = i + 1
-
-					if (i > ending) then
-						ret = true
-						i = i - 1
-					end
-				else
-					table.insert(tbl, {
-						x = x - cos(i * onerad - fixpos) * size,
-						y = y + sin(i * onerad - fixpos) * size
-					})
-
-					i = i - 1
-
-					if (i < 0) then
-						table.insert(tbl, {
-							x = x + cos(fixpos) * size,
-							y = y + sin(fixpos) * size
-						})
-
-						break
-					end
-				end
+			perc = perc - 45
+			if (perc > 90) then
+				insert(tbl, {x = x + w, y = y + h, u = 1, v = 1})
+			else
+				insert(tbl, {x = x + w, y = y - h + perc / 90 * h * 2, u = 1, v = clamp(perc / 90)}) return tbl
 			end
+
+			perc = perc - 90
+			if (perc > 90) then
+				insert(tbl, {x = x - w, y = y + h, u = 0, v = 1})
+			else
+				insert(tbl, {x = x + w - perc / 90 * h * 2, y = y + h, u = 1 - clamp(perc / 90), v = 1}) return tbl
+			end
+
+			perc = perc - 90
+			if (perc > 90) then
+				insert(tbl, {x = x - w, y = y - h, u = 0, v = 0})
+			else
+				insert(tbl, {x = x - w, y = y + h - perc / 90 * h * 2, u = 0, v = 1 - clamp(perc / 90)}) return tbl
+			end
+
+			perc = perc - 90
+			insert(tbl, {x = x - w + perc / 45 * w, y = y - h, u = clamp(perc / 45) / 2, v = 0})
 
 			return tbl
 		end
+	end
 
-		function DrawCorrectFuckingPoly(tbl, fade)
-			local len = #tbl
+	function CreateBorderedCircle(x, y, size, border, perc, addrot, parts)
 
-			for i = 1, #tbl do
+		local sin = math.sin
+		local cos = math.cos
+		local rad = math.rad
 
-				if fade and (i < 5 or i >= (len-5)) then
-					local col = i / 5
+		local parts = parts or 100
 
-					if i > len/2 then
-						col = (len-i-1) / 5
-					end
+		local tbl = {}
+		local onerad = rad(360) / parts
+		local fixpos = rad(90)
+		local ret = false
+		local ending = perc * 3.6 * (parts / 360)
+		local innersize = size - border
+		local i = 0
 
-					surface.SetDrawColor( 175 * col, 225 * col, 100 * col, 200 * col )
-				elseif fade then
-					surface.SetDrawColor( 175, 225, 100, 200 )
+		if (addrot != nil) then
+			fixpos = fixpos + rad(addrot)
+		end
+
+		while(true) do
+			if (not ret) then
+				table.insert(tbl, {
+					x = x - cos(i * onerad - fixpos) * innersize,
+					y = y + sin(i * onerad - fixpos) * innersize
+				})
+
+				i = i + 1
+
+				if (i > ending) then
+					ret = true
+					i = i - 1
 				end
+			else
+				table.insert(tbl, {
+					x = x - cos(i * onerad - fixpos) * size,
+					y = y + sin(i * onerad - fixpos) * size
+				})
 
-				surface.DrawPoly({tbl[i], tbl[len - (i - 1)], tbl[len - i]})
-				surface.DrawPoly({tbl[i], tbl[i + 1], tbl[len - i]})
+				i = i - 1
+
+				if (i < 0) then
+					table.insert(tbl, {
+						x = x + cos(fixpos) * size,
+						y = y + sin(fixpos) * size
+					})
+
+					break
+				end
 			end
 		end
 
-		local function GetAmmo()
-			local ply = LocalPlayer()
-			local weap = ply:GetActiveWeapon()
-			if !IsValid(weap) or not ply:Alive() then return -1 end
+		return tbl
+	end
 
-			local ammo_inv = weap:Ammo1() or 0
-			local ammo_clip = weap:Clip1() or 0
-			local ammo_max = weap.Primary.ClipSize or 0
-			local ammo_invmax = weap.Primary.ClipMax or 0
+	function DrawCorrectFuckingPoly(tbl, fade)
+		local len = #tbl
 
-			return ammo_clip, ammo_max, ammo_inv, ammo_invmax
+		for i = 1, #tbl do
+
+			if fade and (i < 5 or i >= (len-5)) then
+				local col = i / 5
+
+				if i > len/2 then
+					col = (len-i-1) / 5
+				end
+
+				surface.SetDrawColor( 175 * col, 225 * col, 100 * col, 200 * col )
+			elseif fade then
+				surface.SetDrawColor( 175, 225, 100, 200 )
+			end
+
+			surface.DrawPoly({tbl[i], tbl[len - (i - 1)], tbl[len - i]})
+			surface.DrawPoly({tbl[i], tbl[i + 1], tbl[len - i]})
 		end
+	end
 
-		---- Crosshair
-		surface.CreateFont("TargetIDSmall2", {font = "TargetID", size = 16, weight = 1000})
+	local function GetAmmo()
+		local ply = LocalPlayer()
+		local weap = ply:GetActiveWeapon()
+   	if not weap or not ply:Alive() then return -1 end
 
-		local magnifier_mat = Material("icon16/magnifier.png")
-		local ring_tex = surface.GetTextureID("effects/select_ring")
-		local rag_color = Color(200,200,200,255)
+		local ammo_inv = weap:Ammo1() or 0
+		local ammo_clip = weap:Clip1() or 0
+		local ammo_max = weap.Primary.ClipSize or 0
+		local ammo_invmax = weap.Primary.ClipMax or 0
 
-		local delay = 0.075
-		local showtime = 3
-		local client = LocalPlayer()
+		return ammo_clip, ammo_max, ammo_inv, ammo_invmax
+	end
 
-		local margin = 10
-		local width = 350
-		local height = 22
+	---- Crosshair
+	surface.CreateFont("TargetIDSmall2", {font = "TargetID", size = 16, weight = 1000})
 
-		local barcorner = surface.GetTextureID( "gui/corner8" )
-		local round = math.Round
+	local magnifier_mat = Material("icon16/magnifier.png")
+	local ring_tex = surface.GetTextureID("effects/select_ring")
+	local rag_color = Color(200,200,200,255)
 
-		function GetAmmoForCurrentWeapon()
-		  local ply = LocalPlayer()
-		  local wep = ply:GetActiveWeapon()
-		  if ( not wep ) then return 0 end
+	local delay = 0.075
+	local showtime = 3
+	local client = LocalPlayer()
 
-		  local ammo_inv    = ply:GetAmmoCount( wep:GetPrimaryAmmoType() ) or 0
-			local ammo_clip   = wep:Clip1() or 0
-			local ammo_max    = ply:GetAmmoCount( wep:GetPrimaryAmmoType() ) or 0
-			local ammo_invmax = ply:GetAmmoCount( wep:GetSecondaryAmmoType()) or 0
+	local margin = 10
+	local width = 350
+	local height = 22
 
-		  return ammo_clip, ammo_max, ammo_inv, ammo_invmax
-		end
+	local barcorner = surface.GetTextureID( "gui/corner8" )
+	local round = math.Round
+
+	function GetAmmoForCurrentWeapon()
+	  local ply = LocalPlayer()
+	  local wep = ply:GetActiveWeapon()
+	  if ( not wep ) then return 0,0,0,0 end
+	  if ( not IsValid(ply:GetActiveWeapon())) then return 0,0,0,0 end
+
+	  local ammo_inv    = ply:GetAmmoCount( wep:GetPrimaryAmmoType() ) or 0
+		local ammo_clip   = wep:Clip1() or 0
+		local ammo_max    = ply:GetAmmoCount( wep:GetPrimaryAmmoType() ) or 0
+		local ammo_invmax = ply:GetAmmoCount( wep:GetSecondaryAmmoType()) or 0
+
+	  return ammo_clip, ammo_max, ammo_inv, ammo_invmax
+	end
+
+	hook.Add("HUDPaint", "FC_HUD", function()
 
 		GAMEMODE.HUDPaint = function()
 
@@ -297,7 +312,7 @@ else
 			--HEALTH Cricle
 			if LocalPlayer():Alive() then
 				--Fake grey behind
-				local parts = NiceUV(x - wwwwwwwwwww50, y - 67, 64, 64, 360, 0, 100 / 100 * 360, true )
+				local parts = NiceUV(x - 250, y - 67, 64, 64, 360, 0, 100 / 100 * 360, true )
 				surface.SetMaterial( FC_HUD.BigHP )
 				surface.SetDrawColor( 0, 0, 0, 125 )
 				surface.DrawPoly(parts)
@@ -322,6 +337,19 @@ else
 
 			--Health Text inside circle
 			-- draw.SimpleText(LocalPlayer():Health(), "FC_HUD_40", x - 250, y - 67, Color(255,255,255,200), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+			--test
+			local visible_entity = LocalPlayer():GetEyeTrace().Entity
+			local distance_from_object = 85
+			local player_to_entity_distance = LocalPlayer():GetPos():Distance(visible_entity:GetPos())
+
+			if ( input.IsKeyDown( KEY_F ) || input.IsKeyDown( KEY_E ) ) and ( visible_entity:GetClass() == 'payday_duffle_bag') and ( player_to_entity_distance < distance_from_object) then
+				local parts = NiceUV((ScrW())/2, ScrH()/2, 128, 128, 360, 0, 100 / 100 * 360, true )
+				surface.SetMaterial( FC_HUD.BigHP )
+				surface.SetDrawColor(255, 255, 255, 25)
+				surface.DrawPoly(parts)
+			end
+			--
 
 		end
 
